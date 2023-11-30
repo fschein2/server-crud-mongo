cdab4574ba0f6a30e53a9ec94c8f3ba9a350cee9
fschein <fschein02@gmail.com>
2023-11-29 21:48:06 -0500

    connected to mongodb

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const Joi = require("joi");
 const multer = require("multer");
+const mongoose = require("mongoose");
 app.use(express.static("public"));
 app.use(express.json());
 const cors = require("cors");
@@ -9,10 +10,16 @@ app.use(cors());
 
 const upload = multer({ dest: __dirname + "/public/images"});
 
+mongoose
+    .connect("mongodb+srv://fschein21:forMongoOnly@cluster0.n7nc3vs.mongodb.net/?retryWrites=true&w=majority")
+    .then(() => console.log("Connected to mongodb"))
+    .catch((err) => console.log("Couldn't connect to mongodb", err));
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
+/*
 let sodas = [{
         _id: 1,
         name: "Pepsi",
@@ -87,68 +94,84 @@ let sodas = [{
         ],
     }
 ];
+*/
 
-app.get("/api/sodas", (req, res) => {
-    res.send(sodas);
+const sodaSchema = new mongoose.Schema({
+    name: String,
+    sugar: String,
+    calories: String,
+    oz: String,
+    subTypes: [String]
 });
 
+const Soda = mongoose.model("Soda", sodaSchema);
+
+app.get("/api/sodas", (req, res) => {
+    getSodas(res);
+});
+
+const getSodas = async (res) => {
+    const sodas = await Soda.find();
+    res.send(sodas);
+};
+
 app.post("/api/sodas", upload.single("img"), (req, res) => {
-    console.log("Body: " + req.body.sugar);
     const result = validateSoda(req.body);
 
     if (result.error) {
-        res.status(400).send(result.error.details[0].message);
+        res.status(400).send(result.error.deatils[0].message);
         return;
     }
-    console.log(req.body.subTypes);
-    const soda = {
-        _id: sodas.length + 1,
+
+    const soda = new Soda({
         name: req.body.name,
         sugar: req.body.sugar,
         calories: req.body.calories,
         oz: req.body.oz,
-        subTypes: req.body.subTypes.split(",")
-    }
+        subTypes: req.body.subTypes.split(","),
+    });
 
-    sodas.push(soda);
-    res.send(sodas);
+    createSoda(soda, res);
 });
 
+const createSoda = async (soda, res) => {
+    const result = await soda.save();
+    res.send(soda);
+};
+
 app.put("/api/sodas/:id", upload.single("img"), (req, res) => {
-    const id = parseInt(req.params.id);
-
-    const soda = sodas.find((r) => r._id === id);
-
     const result = validateSoda(req.body);
 
-    if (result.error) {
+    if(result.error) {
         res.status(400).send(result.error.details[0].message);
         return;
     }
 
-    soda.name = req.body.name;
-    soda.sugar = req.body.sugar;
-    soda.calories = req.body.calories;
-    soda.oz = req.body.oz;
-    soda.subTypes = req.body.subTypes.split(",");
-
-    res.send(soda);
+    updateSoda(req, res);
 });
 
-app.delete("/api/sodas/:id", upload.single("img"), (req, res) => {
-    const id = parseInt(req.params.id);
-
-    const soda = sodas.find((r) => r._id === id);
-
-    if (!soda) {
-        res.status(404).send("The soda was not found");
-        return;
+const updateSoda = async (req, res) => {
+    let fieldsToUpdate = {
+        name: req.body.name,
+        sugar: req.body.sugar,
+        calories: req.body.calories,
+        oz: req.body.oz,
+        subTypes: req.body.subTypes.split(","),
     }
 
-    const index = sodas.indexOf(soda);
-    sodas.splice(index, 1);
+    const result = await Soda.updateOne({_id: req.params.id}, fieldsToUpdate);
+    const soda = await Soda.findById(req.params.id);
     res.send(soda);
+}
+
+app.delete("/api/sodas/:id", upload.single("img"), (req, res) => {
+    removeSoda(res, req.params.id);
 });
+
+const removeSoda = async (res, id) => {
+    const soda = await Soda.findByIdAndDelete(id);
+    res.send(soda);
+}
 
 const validateSoda = (soda) => {
     const schema = Joi.object({
